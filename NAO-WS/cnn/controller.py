7fefa3681812bbadcf45be9ab01231747fa2325f
Haifeng Jin <jhfjhfj1@gmail.com>
2019-02-16 20:03:41 -0600

    merge encoder into model

--- NAO-WS/cnn/controller.py
+++ NAO-WS/cnn/controller.py
@@ -12,104 +12,14 @@ SOS = 0
 EOS = 0
 
 
-def input_fn(encoder_input, predictor_target, decoder_target, mode, batch_size, num_epochs=1, symmetry=False):
-    shape = np.array(encoder_input).shape
-    if mode == 'train':
-        tf.logging.info('Data size : {}, {}'.format(shape, np.array(predictor_target).shape))
-        N = shape[0]
-        source_length = shape[1]
-        # converting numpy arrays to tf Dataset.
-        encoder_input = tf.convert_to_tensor(encoder_input, dtype=tf.int32)
-        encoder_input = tf.data.Dataset.from_tensor_slices(encoder_input)
-        predictor_target = tf.convert_to_tensor(predictor_target, dtype=tf.float32)
-        predictor_target = tf.data.Dataset.from_tensor_slices(predictor_target)
-        decoder_target = tf.convert_to_tensor(decoder_target, dtype=tf.int32)
-        decoder_target = tf.data.Dataset.from_tensor_slices(decoder_target)
-        dataset = tf.data.Dataset.zip((encoder_input, predictor_target, decoder_target))
-        dataset = dataset.shuffle(buffer_size=N)
-
-        # Add one more element in the dataset, the decoder source.
-        # decoder_src is the input to the lstm decoder, which uses the output of the last step.
-        # Since the first step doesn't have a last step output, we use SOS (start of sequence) = 0 as the input.
-        def preprocess(encoder_src, encoder_tgt, decoder_tgt):  # src:sequence tgt:performance
-            sos_id = tf.constant([SOS])
-            decoder_src = tf.concat([sos_id, decoder_tgt[:-1]], axis=0)
-            return encoder_src, encoder_tgt, decoder_src, decoder_tgt
-
-        def generate_symmetry(encoder_src, encoder_tgt, decoder_src, decoder_tgt):
-            a = tf.random_uniform([], 0, 5, dtype=tf.int32)
-            b = tf.random_uniform([], 0, 5, dtype=tf.int32)
-            cell_seq_length = source_length // 2
-            assert source_length in [40, 60]
-            if source_length == 40:
-                encoder_src = tf.concat(
-                    [encoder_src[:4 * a], encoder_src[4 * a + 2:4 * a + 4], encoder_src[4 * a:4 * a + 2],
-                     encoder_src[4 * (a + 1):cell_seq_length + 4 * b],
-                     encoder_src[cell_seq_length + 4 * b + 2:cell_seq_length + 4 * b + 4],
-                     encoder_src[cell_seq_length + 4 * b:cell_seq_length + 4 * b + 2],
-                     encoder_src[cell_seq_length + 4 * (b + 1):]], axis=0)
-
-            else:  # source_length=60
-                encoder_src = tf.concat(
-                    [encoder_src[:6 * a], encoder_src[6 * a + 3:6 * a + 6], encoder_src[6 * a:6 * a + 3],
-                     encoder_src[6 * (a + 1):cell_seq_length + 6 * b],
-                     encoder_src[cell_seq_length + 6 * b + 3:cell_seq_length + 6 * b + 6],
-                     encoder_src[cell_seq_length + 6 * b:cell_seq_length + 6 * b + 3],
-                     encoder_src[cell_seq_length + 6 * (b + 1):]], axis=0)
-            decoder_tgt = encoder_src
-            return encoder_src, encoder_tgt, decoder_src, decoder_tgt
-
-        dataset = dataset.map(preprocess)
-        if symmetry:
-            dataset = dataset.map(generate_symmetry)
-        dataset = dataset.repeat(num_epochs)
-        dataset = dataset.batch(batch_size)
-        dataset = dataset.prefetch(10)
-        iterator = dataset.make_one_shot_iterator()
-        encoder_input, predictor_target, decoder_input, decoder_target = iterator.get_next()
-        assert encoder_input.shape.ndims == 2
-        assert predictor_target.shape.ndims == 1
-        while predictor_target.shape.ndims < 2:
-            predictor_target = tf.expand_dims(predictor_target, axis=-1)
-        assert decoder_input.shape.ndims == 2
-        assert decoder_target.shape.ndims == 2
-        return encoder_input, predictor_target, decoder_input, decoder_target
-    else:
-        tf.logging.info('Data size : {}'.format(shape))
-        encoder_input = tf.convert_to_tensor(encoder_input, dtype=tf.int32)
-        encoder_input = tf.data.Dataset.from_tensor_slices(encoder_input)
-
-        def preprocess(encoder_src):
-            return encoder_src, tf.constant([SOS], dtype=tf.int32)
-
-        dataset = encoder_input.map(preprocess)
-        dataset = dataset.repeat(num_epochs)
-        dataset = dataset.batch(batch_size)
-        dataset = dataset.prefetch(10)
-        iterator = dataset.make_one_shot_iterator()
-        encoder_input, decoder_input = iterator.get_next()
-        assert encoder_input.shape.ndims == 2
-        return encoder_input, decoder_input
-
-
-def get_train_ops(encoder_train_input, predictor_train_target, decoder_train_input, decoder_train_target, params,
+def get_train_ops(embedding, predictor_train_target, params,
                   reuse=tf.AUTO_REUSE):
     with tf.variable_scope('EPD', reuse=reuse):
-        my_encoder = encoder.Model(encoder_train_input, predictor_train_target, params, tf.estimator.ModeKeys.TRAIN,
-                                   'Encoder', reuse)
-        encoder_outputs = my_encoder.encoder_outputs
-        encoder_state = my_encoder.arch_emb
-        encoder_state.set_shape([None, params['decoder_hidden_size']])
-        encoder_state = tf.contrib.rnn.LSTMStateTuple(encoder_state, encoder_state)
-        encoder_state = (encoder_state,) * params['decoder_num_layers']
-        my_decoder = decoder.Model(encoder_outputs, encoder_state, decoder_train_input, decoder_train_target, params,
-                                   tf.estimator.ModeKeys.TRAIN, 'Decoder', reuse)
-        encoder_loss = my_encoder.loss
-        decoder_loss = my_decoder.loss
-        mse = encoder_loss
-        cross_entropy = decoder_loss
+        predictor = encoder.Predictor(params)
+        predictor.build(embedding)
+        predictor.compute_loss(predictor_train_target)
 
-        total_loss = params['trade_off'] * encoder_loss + (1 - params['trade_off']) * decoder_loss + params[
+        total_loss = predictor.loss + params[
             'weight_decay'] * tf.add_n(
             [tf.nn.l2_loss(v) for v in tf.trainable_variables()])
 
@@ -144,7 +54,7 @@ def get_train_ops(encoder_train_input, predictor_train_target, decoder_train_inp
             train_op = opt.apply_gradients(
                 zip(clipped_gradients, variables), global_step=global_step)
 
-        return mse, cross_entropy, total_loss, learning_rate, train_op, global_step, grad_norm
+        return total_loss, learning_rate, train_op, global_step, grad_norm
 
 
 def get_predict_ops(encoder_predict_input, decoder_predict_input, params, reuse=tf.AUTO_REUSE):
@@ -176,21 +86,19 @@ def get_predict_ops(encoder_predict_input, decoder_predict_input, params, reuse=
         return predict_value, sample_id, new_sample_id
 
 
+def input_fn(embedding, predictor_target, batch_size):
+    return None, None
+
+
 # encoder target is the list of performances of the architectures
-def train(params, encoder_input, predictor_target, decoder_target):
+def train(params, embedding, predictor_target):
     with tf.Graph().as_default():
         tf.logging.info('Training Encoder-Predictor-Decoder')
         tf.logging.info('Preparing data')
-        shape = np.array(encoder_input).shape
-        N = shape[0]
-        encoder_train_input, predictor_train_target, decoder_train_input, decoder_train_target = input_fn(
-            encoder_input,
+        embedding, predictor_train_target = input_fn(
+            embedding,
             predictor_target,
-            decoder_target,
-            'train',
             params['batch_size'],
-            None,
-            params['symmetry'],
         )
         tf.logging.info('Building model')
         train_mse, train_cross_entropy, train_loss, learning_rate, train_op, global_step, grad_norm = get_train_ops(
--- NAO-WS/cnn/encoder.py
+++ NAO-WS/cnn/encoder.py
@@ -9,8 +9,66 @@ _BATCH_NORM_DECAY = 0.9
 _BATCH_NORM_EPSILON = 1e-5
 
 
+class Predictor:
+    def __init__(self, params):
+        self.emb_size = params['encoder_emb_size']
+        self.mlp_num_layers = params['mlp_num_layers']
+        self.mlp_hidden_size = params['mlp_hidden_size']
+        self.mlp_dropout = params['mlp_dropout']
+        self.dropout = params['encoder_dropout']
+        self.output = None
+        self.loss = None
+
+    def build(self, x):
+        for i in range(self.mlp_num_layers):
+            name = 'mlp_{}'.format(i)
+            x = tf.layers.dense(x, self.mlp_hidden_size, activation=tf.nn.relu, name=name)
+            x = tf.layers.dropout(x, self.mlp_dropout)
+        self.output = tf.layers.dense(x, 1, activation=tf.sigmoid, name='regression')
+
+    def compute_loss(self, target):
+        pass
+
+
 class Encoder(object):
-    def __init__(self, params, mode, W_emb):
+    def __init__(self, x, params, mode, scope='Encoder', reuse=tf.AUTO_REUSE):
+        self.x = x
+        self.params = params
+        self.batch_size = tf.shape(x)[0]
+        self.vocab_size = params['encoder_vocab_size']
+        self.emb_size = params['encoder_emb_size']
+        self.hidden_size = params['encoder_hidden_size']
+        self.encoder_length = params['encoder_length']
+        self.weight_decay = params['weight_decay']
+        self.mode = mode
+        self.time_major = params['time_major']
+        self.is_training = self.mode == tf.estimator.ModeKeys.TRAIN
+        if not self.is_training:
+            self.params['encoder_dropout'] = 0.0
+            self.params['mlp_dropout'] = 0.0
+
+        # initializer = tf.orthogonal_initializer()
+        initializer = tf.random_uniform_initializer(-0.1, 0.1)
+        tf.get_variable_scope().set_initializer(initializer)
+        self.build_graph(scope=scope, reuse=reuse)
+
+
+
+    def build_graph(self, scope=None, reuse=tf.AUTO_REUSE):
+        tf.logging.info("# creating %s graph ..." % self.mode)
+        # Encoder
+        with tf.variable_scope(scope, reuse=reuse):
+            self.W_emb = tf.get_variable('W_emb', [self.vocab_size, self.emb_size])
+            self.arch_emb, self.encoder_outputs, self.encoder_state = self.build_encoder()
+            if self.mode != tf.estimator.ModeKeys.PREDICT:
+                self.compute_loss()
+            else:
+                self.loss = None
+                self.total_loss = None
+
+    def build_encoder(self):
+        params = self.params
+        #encoder init
         self.num_layers = params['encoder_num_layers']
         self.hidden_size = params['encoder_hidden_size']
         self.emb_size = params['encoder_emb_size']
@@ -22,10 +80,10 @@ class Encoder(object):
         self.vocab_size = params['encoder_vocab_size']
         self.dropout = params['encoder_dropout']
         self.time_major = params['time_major']
-        self.W_emb = W_emb
-        self.mode = mode
 
-    def build_encoder(self, x, batch_size, is_training):
+        x = self.x
+        batch_size = self.batch_size
+
         self.batch_size = batch_size
         assert x.shape.ndims == 2, '[batch_size, length]'
         x = tf.gather(self.W_emb, x)
@@ -72,50 +130,12 @@ class Encoder(object):
         momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON,
         center=True, scale=True, training=is_training, fused=True
         )'''
-        return {
+        res = {
             'arch_emb': self.arch_emb,
             'encoder_outputs': self.encoder_outputs,
             'encoder_state': self.encoder_state,
         }
 
-
-class Model(object):
-    def __init__(self, x, params, mode, scope='Encoder', reuse=tf.AUTO_REUSE):
-        self.x = x
-        self.params = params
-        self.batch_size = tf.shape(x)[0]
-        self.vocab_size = params['encoder_vocab_size']
-        self.emb_size = params['encoder_emb_size']
-        self.hidden_size = params['encoder_hidden_size']
-        self.encoder_length = params['encoder_length']
-        self.weight_decay = params['weight_decay']
-        self.mode = mode
-        self.time_major = params['time_major']
-        self.is_training = self.mode == tf.estimator.ModeKeys.TRAIN
-        if not self.is_training:
-            self.params['encoder_dropout'] = 0.0
-            self.params['mlp_dropout'] = 0.0
-
-        # initializer = tf.orthogonal_initializer()
-        initializer = tf.random_uniform_initializer(-0.1, 0.1)
-        tf.get_variable_scope().set_initializer(initializer)
-        self.build_graph(scope=scope, reuse=reuse)
-
-    def build_graph(self, scope=None, reuse=tf.AUTO_REUSE):
-        tf.logging.info("# creating %s graph ..." % self.mode)
-        # Encoder
-        with tf.variable_scope(scope, reuse=reuse):
-            self.W_emb = tf.get_variable('W_emb', [self.vocab_size, self.emb_size])
-            self.arch_emb, self.encoder_outputs, self.encoder_state = self.build_encoder()
-            if self.mode != tf.estimator.ModeKeys.PREDICT:
-                self.compute_loss()
-            else:
-                self.loss = None
-                self.total_loss = None
-
-    def build_encoder(self):
-        encoder = Encoder(self.params, self.mode, self.W_emb)
-        res = encoder.build_encoder(self.x, self.batch_size, self.is_training)
         return res['arch_emb'], res['encoder_outputs'], res['encoder_state']
 
     def compute_loss(self):
